bf8c5f739ec830edff9a25a779c8ff7d60de0af5
Amanda Chen <cychen@inchmountain-lm.tpcity.corp.yahoo.com>
2014-02-12 13:28:06 +0800

    fix js errors regarding to waterfall and run

--- public/javascripts/wpt-testing.js
+++ public/javascripts/wpt-testing.js
@@ -141,7 +141,7 @@ wpt.prototype.renderHtmlInfo = function(){
 }
 wpt.prototype.renderHtmlEntry = function(data){
     var content = '<tr>';
-    content +=  '<td align="left">' + data.run || '' + '</td>' +
+    content +=  '<td align="left">' + (data.run || '') + '</td>' +
                 '<td id="fvLoadTime">' + (data.loadTime/1000).toFixed(3) + 's</td>' +
                 '<td id="fvTTFB">' + (data.TTFB/1000).toFixed(3) + 's</td>' +
                 '<td id="fvStartRender">' + (data.render/1000).toFixed(3) + 's</td>' +
@@ -176,7 +176,6 @@ wpt.prototype.renderHtmlResult = function(data){
     content += this.renderHtmlEntry(data.median.firstView);
     content += '</table>';
     content += '<hr>';
-    content += '<img scr="' + data.median.images.waterfall + '">';
     return content;
 };
 
